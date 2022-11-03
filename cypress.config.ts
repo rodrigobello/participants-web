140eb4be1dc22b5bb647e6627d9cebc9f18ac664
Rodrigo Bello <rodrigoabbello@gmail.com>
2022-11-03 01:22:15 -0300

    Refactor project tests structure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: false,
   e2e: {
+    specPattern: "src/tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/participants-web/",
   resolve: {
     alias: {
       "@participant": path.resolve(__dirname, "./src/apps/participant"),
