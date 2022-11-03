cffaafdde00f64de0afac1a79ea039f4e8c117df
Rodrigo Bello <rodrigoabbello@gmail.com>
2022-11-03 01:09:04 -0300

    Update home screen description text

--- src/apps/participant/components/ParticipantsEndpointInput.vue
+++ src/apps/participant/components/ParticipantsEndpointInput.vue
@@ -36,10 +36,13 @@ const valid = computed(() =>
             d="M34 40h10v-4a6 6 0 00-10.712-3.714M34 40H14m20 0v-4a9.971 9.971 0 00-.712-3.714M14 40H4v-4a6 6 0 0110.713-3.714M14 40v-4c0-1.313.253-2.566.713-3.714m0 0A10.003 10.003 0 0124 26c4.21 0 7.813 2.602 9.288 6.286M30 14a6 6 0 11-12 0 6 6 0 0112 0zm12 6a4 4 0 11-8 0 4 4 0 018 0zm-28 0a4 4 0 11-8 0 4 4 0 018 0z"
           />
         </svg>
-        <h2 class="mt-2 text-lg font-medium text-gray-900">Participants API</h2>
+        <h2 class="mt-2 text-lg font-medium text-gray-900">
+          Open Banking Brasil Participants
+        </h2>
         <p class="mt-2 text-sm font-light text-gray-400">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi
-          velit, porttitor quis consequat in, facilisis nec nisl.
+          By loading a valid "participants API endpoint", we can display all
+          relevant information for Open Banking Brasil participants registered
+          in the Raidiam directory platform.
         </p>
       </div>
       <div class="mt-6 flex">
